10e3d1a298550e1a29b53947bde99640e8d30b24
Josh Winter <winterjoshr@gmail.com>
2018-05-01 16:47:14 -0400

    windows msvc support, and test driver (test.cpp)

---
+++ test.cpp
@@ -0,0 +1,13 @@
+#include "press.h"
+
+int main()
+{
+	press::write("\"the year is {}\"\n", 2018);
+	press::write("\"the year is {5}\"\n", 2018);
+	press::write("\"the year is {05}\"\n", 2018);
+	press::write("\"the year is {-5}\"\n", 2018);
+	press::write("\"pi is {.4}\"\n", 3.1415926);
+	press::write("\"my name is {}\"\n", "Bob");
+
+	return 0;
+}
